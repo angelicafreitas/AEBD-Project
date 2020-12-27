abad6f4652e211160790ca3a418549dc0c8f3b7e
beatrizfrocha <47789897+beatrizfrocha@users.noreply.github.com>
2020-12-27 20:19:53 +0000

    add sql script and model

---
+++ database/AEBD_TP_CONNECTION.sql
@@ -0,0 +1,66 @@
+create table db (
+  db_id int not null enable,
+  database_name varchar2(200),
+  instance_name varchar2(200),
+  "version" varchar2(200),
+  CONSTRAINT db_PK PRIMARY KEY (db_id)
+);
+
+create table sql_monitor (
+    sql_monitor_id int not null enable,
+    db_id int,
+    sql_text varchar2(200),
+    pdb varchar2(200),
+    query_date timestamp,
+    CONSTRAINT SQL_MONITOR_PK PRIMARY KEY (sql_monitor_id),
+    CONSTRAINT SQL_MONITOR_FK FOREIGN KEY (db_id) REFERENCES db(db_id)
+);
+
+create table "users" (
+    user_id int not null enable,
+    db_id int,
+    user_name varchar2(200),
+    default_tablespace varchar2(200),
+    temporary_tablespace varchar2(200),
+    query_date timestamp,
+    CONSTRAINT USER_PK PRIMARY KEY (user_id),
+    CONSTRAINT USER_FK FOREIGN KEY (db_id) REFERENCES db(db_id)
+);
+
+create table "privileges" (
+    privilege_id int not null enable,
+    name varchar2(200),
+    CONSTRAINT PRIVILEGE_PK PRIMARY KEY (privilege_id)
+);
+
+create table users_privileges (
+    users_privileges_id int not null enable,
+    user_id int,
+    privilege_id int,
+    CONSTRAINT USERS_PRIVILEGES_PK PRIMARY KEY (users_privileges_id),
+    CONSTRAINT USERS_PRIVILEGES_FK1 FOREIGN KEY (user_id) REFERENCES "users"(user_id),
+    CONSTRAINT USERS_PRIVILEGES_FK2 FOREIGN KEY (privilege_id) REFERENCES "privileges"(privilege_id)
+);
+
+create table "tablespaces" (
+    tablespace_id int not null enable,
+    db_id int,
+    tablespace_name varchar2(200),
+    "Allocated (MB)" int,
+    "Free (MB)" int,
+    "Used (MB)" int,
+    query_date timestamp,
+    CONSTRAINT TABLESPACE_PK PRIMARY KEY (tablespace_id),
+    CONSTRAINT TABLESPACE_FK FOREIGN KEY (db_id) REFERENCES db(db_id)
+);
+
+create table "datafiles" (
+    datafile_id int not null enable,
+    tablespace_id int,
+    file_name varchar2(200),
+    "Size (MB)" float,
+    "Used (MB)" float,
+    query_date timestamp,
+    CONSTRAINT DATAFILE_PK PRIMARY KEY (datafile_id),
+    CONSTRAINT DATAFILE_FK FOREIGN KEY (tablespace_id) REFERENCES "tablespaces"(tablespace_id)
+);
