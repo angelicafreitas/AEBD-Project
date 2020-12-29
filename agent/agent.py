ad06a68c2f8f7919a07ff79ba8e869534d304171
angelicafreitas <angelicamfreitas09@gmail.com>
2020-12-29 16:29:36 +0000

    more agent progress

--- agent/agent.py
+++ agent/agent.py
@@ -32,60 +32,122 @@ if sys.argv.__len__() == 5:
     try:
         with orc.connect(USERNAME, PASSWORD, DSN, encoding=ENCODING) as db:
             with db.cursor() as cursor:
-                cursor.execute(f'select sysdate from dual')
-                res, = cursor.fetchone()
-                print(f'\t > {res}\n')
+
+                # # Data needed for Table "DB" -----------------------------------
+                # print(' > Fetching data for table DB...')
+                # fetch_db_info = """
+                #     SELECT (select name from V$database) "Database name" ,
+                #             sys_context('userenv','instance_name') "Instance name",
+                #             (SELECT version FROM V$INSTANCE) "Version name" 
+                #     FROM dual
+                # """
+                # cursor.execute(fetch_db_info)
+                # database_name, instance_name, version_name = cursor.fetchone()
+                
+                # print(f'\tDatabase name: {database_name}')
+                # print(f'\tInstance name: {instance_name}')
+                # print(f'\tVersion name: {version_name}')
+
+
+                # #Data needed for table "TABLESPACE" -----------------------------------
+                # print('\n')
+                # print(' > Fetching data for table TABLESPACE...')
+                # fetch_tablespace_info = """
+                #     SELECT TABLESPACE_NAME "NAME",
+                #             Round(TABLESPACE_SIZE * (SELECT VALUE FROM v$parameter v WHERE name = 'db_block_size')/1024/1024,0) "sizeMB",
+                #             Round((TABLESPACE_SIZE-USED_SPACE)* (SELECT VALUE FROM v$parameter v WHERE name = 'db_block_size')/1024/1024,0) "free",
+                #             Round(USED_SPACE * (SELECT VALUE FROM v$parameter v WHERE name = 'db_block_size')/1024/1024,0) "used",
+                #             CURRENT_TIMESTAMP  "query_date"
+                #     FROM DBA_TABLESPACE_USAGE_METRICS
+                # """
+                # cursor.execute(fetch_tablespace_info)
+                # for tablespace_name, sizeMB, free, used, query_date in cursor:             
+                #     print(f'\tTablespace name: {tablespace_name}')
+                #     print(f'\tSize: {sizeMB}')
+                #     print(f'\tFree (MB): {free}')
+                #     print(f'\tUsed (MB): {used}')
+                #     print(f'\tQuery date: {query_date}')
+                #     print(f'\t-----')                
 
                 
                 
+                # #Data needed for Table "DATAFILES" -----------------------------------
+                # print('\n')
+                # print(' > Fetching data for table DATAFILES...')
+                # fetch_datafile_info = """
+                #     SELECT  df.FILE_ID "file_id",
+                #             Substr(df.file_name,1,80) "file_name",
+                #             Substr(df.tablespace_name,1,20) "tablespace_name",
+                #             Round(df.bytes/1024/1024,0) "sizeMB",
+                #             decode(f.free_bytes,NULL,0,Round(f.free_bytes/1024/1024,0)) "free",
+                #             decode(e.used_bytes,NULL,0,Round(e.used_bytes/1024/1024,0)) "used",
+                #             CURRENT_TIMESTAMP "query_date"
+                #     FROM    DBA_DATA_FILES DF,
+                #         (SELECT file_id,
+                #                 sum(bytes) used_bytes
+                #             FROM dba_extents
+                #             GROUP by file_id) E,
+                #         (SELECT sum(bytes) free_bytes,
+                #                 file_id
+                #             FROM dba_free_space
+                #             GROUP BY file_id) f
+                #     WHERE    e.file_id (+) = df.file_id
+                #     AND      df.file_id  = f.file_id (+)
+                #     ORDER BY df.tablespace_name,
+                #             df.file_name               
+                # """                
+                # cursor.execute(fetch_datafile_info)
                 
-                # select v.SQL_FULLTEXT, v.FIRST_LOAD_TIME, v.CON_ID, v.ELAPSED_TIME, v.EXECUTIONS, v.SERVICE, v.PARSING_USER_ID 
-                # FROM v$sql v
-                # WHERE v.SERVICE='orclpdb1.localdomain'
-                # AND v.PARSING_USER_ID!=0
-                # ORDER BY v.FIRST_LOAD_TIME DESC;
-
-
-
-                #Data needed for tablespace -----------------------------------
-                # SELECT TABLESPACE_NAME "NAME",
-                #         Round(TABLESPACE_SIZE * (SELECT VALUE FROM v$parameter v WHERE name = 'db_block_size')/1024/1024,0) "Size (M)",
-                #         Round((TABLESPACE_SIZE-USED_SPACE)* (SELECT VALUE FROM v$parameter v WHERE name = 'db_block_size')/1024/1024,0) "FREE (M)",
-                #         Round(USED_SPACE * (SELECT VALUE FROM v$parameter v WHERE name = 'db_block_size')/1024/1024,0) "USED (M)",
-                #         CURRENT_TIMESTAMP  
-                # FROM DBA_TABLESPACE_USAGE_METRICS;
-
-                #Data needed for datafiles -----------------------------------
-                # SELECT  df.FILE_ID,
-                #         Substr(df.file_name,1,80) "File Name",
-                #         Substr(df.tablespace_name,1,20) "Tablespace Name",
-                #         Round(df.bytes/1024/1024,0) "Size (M)",
-                #         decode(f.free_bytes,NULL,0,Round(f.free_bytes/1024/1024,0)) "Free (M)",
-                #         decode(e.used_bytes,NULL,0,Round(e.used_bytes/1024/1024,0)) "Used (M)",
-                #         CURRENT_TIMESTAMP
-                # FROM    DBA_DATA_FILES DF,
-                #     (SELECT file_id,
-                #             sum(bytes) used_bytes
-                #         FROM dba_extents
-                #         GROUP by file_id) E,
-                #     (SELECT sum(bytes) free_bytes,
-                #             file_id
-                #         FROM dba_free_space
-                #         GROUP BY file_id) f
-                # WHERE    e.file_id (+) = df.file_id
-                # AND      df.file_id  = f.file_id (+)
-                # ORDER BY df.tablespace_name,
-                #         df.file_name;
+                # for file_id, file_name, tablespace_name, sizeMB, free, used, query_date in cursor:
+                #     print(f'\tFile id: {file_id}')
+                #     print(f'\tFile name: {file_name}')
+                #     print(f'\tTablespace name: {tablespace_name}')
+                #     print(f'\tSize: {sizeMB}')
+                #     print(f'\tFree (MB): {free}')
+                #     print(f'\tUsed (MB): {used}')
+                #     print(f'\tQuery date: {query_date}')
+                #     print(f'\t-----')                
                 
-                # Data needed for Users -----------------------------------
-                # SELECT USER_ID, USERNAME, DEFAULT_TABLESPACE, TEMPORARY_TABLESPACE, CURRENT_TIMESTAMP
-                # FROM DBA_USERS;
-
-                # All privleges
-                # SELECT PRIVILEGE, NAME, PROPERTY
-                # FROM SYSTEM_PRIVILEGE_MAP;              
                 
-                # All privileges from Users
+                
+                # # Data needed for Users -----------------------------------
+                # print(' > Fetching data for table USERS...')
+                # fetch_user_info = """
+                #     SELECT USER_ID,
+                #             USERNAME,
+                #             DEFAULT_TABLESPACE,
+                #             TEMPORARY_TABLESPACE,
+                #             CURRENT_TIMESTAMP
+                #     FROM DBA_USERS
+                # """
+                # cursor.execute(fetch_user_info)
+                
+                # for user_id, user_name, default_tablespace, temporary_tablespace, query_date in cursor:
+                #     print(f'\tUser id: {user_id}')
+                #     print(f'\tUser name: {user_name}')
+                #     print(f'\tDefault tablespace: {default_tablespace}')
+                #     print(f'\tTemporary tablespace: {temporary_tablespace}')
+                #     print(f'\tQuery date: {query_date}')
+                #     print(f'\t-----')                
+                
+
+                # Data needed for Table "PRIVILEGES"
+                # print(' > Fetching data for table PRIVILEGES...')
+                # fetch_privilege_info = """
+                #     SELECT PRIVILEGE, NAME, PROPERTY
+                #     FROM SYSTEM_PRIVILEGE_MAP         
+                # """
+                # cursor.execute(fetch_privilege_info)
+                
+                # for privilege_id, name, property in cursor:
+                #     print(f'\tPrivilege id: {privilege_id}')
+                #     print(f'\tName: {name}')
+                #     print(f'\tProperty: {property}')
+                #     print(f'\t-----')                
+
+                #----------------------------------------------------------------------------------
+                
+                # Data needed for Table "USERS"
                 # SELECT d1.USERNAME, d.GRANTEE, d.PRIVILEGE, CURRENT_TIMESTAMP 
                 # FROM dba_sys_privs d, DBA_USERS d1   
                 # WHERE grantee = d1.USERNAME
@@ -95,11 +157,6 @@ if sys.argv.__len__() == 5:
                 #     CONNECT BY PRIOR granted_role = grantee 
                 #     START WITH grantee = d1.USERNAME
                 # ) ORDER BY 1;              
-
-
-
-                # Data needed for Info -----------------------------------
-                # SELECT (SELECT sys_context('userenv','instance_name') FROM dual) "Instance name" , (select name from V$database) "Database name" , (SELECT version FROM V$INSTANCE) "Version name" FROM dual;                
 
 
                 # Data needed for CPU -----------------------------------
@@ -143,3 +200,12 @@ try:
             print(f'> From AEBDPDB {res}')
 except Exception as e:
     print(e)
+
+
+
+
+# select v.SQL_FULLTEXT, v.FIRST_LOAD_TIME, v.CON_ID, v.ELAPSED_TIME, v.EXECUTIONS, v.SERVICE, v.PARSING_USER_ID 
+# FROM v$sql v
+# WHERE v.SERVICE='orclpdb1.localdomain'
+# AND v.PARSING_USER_ID!=0
+# ORDER BY v.FIRST_LOAD_TIME DESC;
